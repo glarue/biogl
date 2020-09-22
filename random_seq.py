810d45ddc8f472e07b36b4e4df8f4babe30dbbca
Graham Larue <egrahamlarue@gmail.com>
2020-09-22 11:28:31 -0700

    Add random seed argument

--- random_seq.py
+++ random_seq.py
@@ -1,10 +1,13 @@
 import random
 
-def random_seq(n, characters='ACTG'):
+def random_seq(n, characters='ACTG', seed=None):
     """
     Returns a sequence of length >n<, composed
     of a random combination of characters chosen
     from >characters<. 
     
     """
+    if seed:
+        random.seed(seed)
+
     return ''.join(random.choices(characters, k=n))
