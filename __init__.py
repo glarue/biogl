a8ca2a285c928649f084b14dc314e8a15d8e4833
Graham Larue <egrahamlarue@gmail.com>
2025-04-01 20:21:05 +0000

    Update project template info

--- __init__.py
+++ __init__.py
@@ -1,18 +1,17 @@
-# import various functions here to allow for easy import
-# from the biogl module (e.g. from biogl import {func})
+__version__ = "2.4.0"
 
+from biogl.arg_info_header import arg_info_header
+from biogl.byte_convert import byte_convert
+from biogl.coord_overlap import coord_overlap
 from biogl.fasta_parse import fasta_parse
+from biogl.file_hash import file_hash
+from biogl.flex_open import flex_open
+from biogl.get_runtime import get_runtime
 from biogl.GxfParse import GxfParse
+from biogl.is_fasta import is_fasta
+from biogl.label_info import label_info
+from biogl.random_seq import random_seq
+from biogl.recursive_items import recursive_items
 from biogl.rev_comp import rev_comp
 from biogl.translate import translate
-from biogl.flex_open import flex_open
-from biogl.random_seq import random_seq
-from biogl.get_runtime import get_runtime
-from biogl.byte_convert import byte_convert
-from biogl.label_info import label_info
-from biogl.is_fasta import is_fasta
-from biogl.recursive_items import recursive_items
 from biogl.window import window
-from biogl.arg_info_header import arg_info_header
-from biogl.coord_overlap import coord_overlap
-from biogl.file_hash import file_hash
